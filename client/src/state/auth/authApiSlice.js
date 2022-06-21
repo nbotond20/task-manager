8ba0ec36d1ff9434faf498c9ff52184375525dbd
nbotond20 <nuszplbotond@gmail.com>
2022-06-21 14:16:54 +0200

    Build

--- client/src/state/auth/authApiSlice.js
+++ client/src/state/auth/authApiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = 'http://localhost:3030/';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
